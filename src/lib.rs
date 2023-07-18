da75ba26dbf9147cffad67b9638bc92764e77d67
dewmal <dewmal@syigen.com>
2023-07-18 16:42:48 +0530

    init agent

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+
+#[pyclass]
+struct Agent {
+    name: String,
 }
+
+#[pymethods]
+impl Agent {
+    #[new]
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+
+    fn get_name(&self) -> String {
+        self.name.to_string()
+    }
+}
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rakun(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Agent>()?;
     Ok(())
 }
--- tests/rakun_main_test.py
+++ tests/rakun_main_test.py
@@ -5,8 +5,8 @@ import rakun
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        res = rakun.rakun.sum_as_string(1, 2)
-        self.assertEqual(res, "3")
+        res = rakun.rakun.Agent("test")
+        assert res.get_name() == "test"
 
 
 if __name__ == '__main__':
