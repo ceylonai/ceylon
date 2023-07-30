36b2e73d063dd54d4be9a7b5803b17302f6537ba
dewmal <dewmal@syigen.com>
2023-07-30 08:08:45 +0530

    v0.1.14

--- examples/greeting_agents/tic_tac_game.py
+++ examples/greeting_agents/tic_tac_game.py
@@ -7,6 +7,41 @@ logging.basicConfig(level=logging.INFO)
 from rk_core import AgentManager, EventType, Event, Processor
 
 
+class GameState:
+    def __init__(self):
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.current_player = None
+
+    def switch_player(self, player1, player2):
+        if self.current_player is None:
+            self.current_player = player1
+        elif self.current_player == player1:
+            self.current_player = player2
+        else:
+            self.current_player = player1
+
+    def make_move(self, x, y):
+        if self.board[x][y] == " " and self.current_player is not None:
+            self.board[x][y] = self.current_player.symbol
+            return True
+        else:
+            return False
+
+    def is_game_over(self):
+        for row in self.board:
+            if row[0] != " " and row[0] == row[1] and row[0] == row[2]:
+                return True
+        for col in range(3):
+            if self.board[0][col] != " " and self.board[0][col] == self.board[1][col] and self.board[0][col] == \
+                    self.board[2][col]:
+                return True
+        if self.board[0][0] != " " and self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]:
+            return True
+        if self.board[0][2] != " " and self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0]:
+            return True
+        return False
+
+
 #
 class PlayerAgent:
     def __init__(self, name, symbol):
@@ -15,7 +50,8 @@ class PlayerAgent:
 
     @Processor(event_type=EventType.Data)
     async def get_game_board_status(self, event: Event):
-        print(f"{self.name} received: {event.content}")
+        board = orjson.loads(bytearray(event.content))
+        print(f"{self.name} received: {board}")
 
 
 class GameBoardAgent:
@@ -31,46 +67,12 @@ class GameBoardAgent:
             await asyncio.sleep(1)
             board = orjson.dumps(self.board)
             print(f"{self.name} board: {board}")
-            self.publisher.publish(f"{board}")
+            self.publisher.publish(board)
             print("GameBoardAgent on_start")
 
     @Processor(event_type=EventType.OnShutdown)
     async def on_shutdown(self):
         print(f"EchoAgent Bye, world! {self.name}")
-
-    # @Processor(event_type=EventType.Data)
-    # async def update_board(self, event: Event):
-    #     self.current_player, move = event.content
-    #     x, y = move
-    #     if self.board[x][y] == " ":
-    #         self.board[x][y] = self.current_player.symbol
-    #         if self.check_for_winner():
-    #             print(f"{self.current_player.name} won!")
-    #             await self.publisher.publish((self.current_player.name, "won"))
-    #         else:
-    #             print(f"{self.current_player.name} made a move at {move}.")
-    #             await self.publisher.publish("next_turn")
-    #     else:
-    #         # Invalid move, ask the player to move again\
-    #         print(f"{self.current_player.name} couldn't make a move. The game might be over.")
-    #         await self.publisher.publish((self.current_player.name, "invalid_move"))
-    #
-    # def check_for_winner(self):
-    #     # Check rows
-    #     for i in range(3):
-    #         if self.board[i][0] == self.board[i][1] == self.board[i][2] != " ":
-    #             return True
-    #     # Check columns
-    #     for i in range(3):
-    #         if self.board[0][i] == self.board[1][i] == self.board[2][i] != " ":
-    #             return True
-    #     # Check diagonals
-    #     if self.board[0][0] == self.board[1][1] == self.board[2][2] != " " or \
-    #             self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
-    #         return True
-    #
-    #     # If no one has won yet, return False
-    #     return False
 
 
 if __name__ == "__main__":
