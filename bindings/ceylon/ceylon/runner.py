8c5103fa67910b1f1cef8b236e7540ebb8bd9b58
dewmal <dewmal@syigen.com>
2024-06-12 01:39:12 +0530

    Write test case for agent runner.py

--- bindings/ceylon/ceylon/runner.py
+++ bindings/ceylon/ceylon/runner.py
@@ -1,20 +1,40 @@
 import asyncio
 
-from ceylon.ceylon import Workspace, WorkspaceConfig, uniffi_set_event_loop
+from ceylon.ceylon import Workspace, WorkspaceConfig, uniffi_set_event_loop, AgentCore
+
+
+class AgentRunnerError(Exception):
+    pass
+
+
+class AgentRunnerNotLeaderError(AgentRunnerError):
+    pass
+
+
+class AgentRunnerCannotHaveMultipleLeadersError(AgentRunnerError):
+    pass
 
 
 class AgentRunner:
     agents = []
     config: WorkspaceConfig
+    leader_agent = None
 
     def __init__(self, workspace_name, workspace_host="/ip4/0.0.0.0/tcp", workspace_port=8888):
         self.agents = []
         self.config = WorkspaceConfig(name=workspace_name, host=workspace_host, port=workspace_port)
 
-    def register_agent(self, agent):
+    def register_agent(self, agent: AgentCore):
+        # Not accepting multiple leaders
+        if self.leader_agent is None and agent.is_leader():
+            self.leader_agent = agent
+        elif agent.is_leader() and self.leader_agent is not None:
+            raise AgentRunnerCannotHaveMultipleLeadersError()
         self.agents.append(agent)
 
     async def run(self, inputs):
+        if self.leader_agent is None:
+            raise AgentRunnerNotLeaderError()
         uniffi_set_event_loop(asyncio.get_event_loop())
         workspace = Workspace(agents=self.agents, config=self.config)
         await workspace.run(inputs)
--- bindings/ceylon/tests/llm_agen_test.py
+++ bindings/ceylon/tests/llm_agen_test.py
@@ -1,14 +1,19 @@
 import asyncio
 
 from ceylon import AgentRunner, LLMAgent
+from ceylon.llm_agent import LLMManager
 
 
 async def main():
     runner = AgentRunner(workspace_name="ceylon-ai")
-    runner.register_agent(LLMAgent(name="writer", is_leader=True, ))
+    runner.register_agent(LLMManager())
+    runner.register_agent(LLMAgent(name="writer"))
+    runner.register_agent(LLMAgent(name="researcher"))
     await runner.run({
         "title": "How to use AI for Machine Learning",
     })
+    leader = runner.get_leader()
+    print(leader.report)
 
 
 if __name__ == '__main__':
