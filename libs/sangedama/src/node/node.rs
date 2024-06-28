62a4c70d8f33cfe10fd40b1290e5c0f7e09b6626
dewmal <dewmal@syigen.com>
2024-06-28 10:44:43 +0530

    Update node

--- libs/sangedama/src/node/node.rs
+++ libs/sangedama/src/node/node.rs
@@ -1,7 +1,9 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::time::{Duration, SystemTime};
 
-use libp2p::{futures::StreamExt, gossipsub, mdns, swarm::{NetworkBehaviour, Swarm, SwarmEvent}, SwarmBuilder, tcp, noise, yamux};
+use libp2p::{futures::StreamExt, gossipsub, mdns, swarm::{NetworkBehaviour, Swarm, SwarmEvent}, SwarmBuilder, tcp, noise, yamux, PeerId, tls};
+use libp2p::core::muxing::StreamMuxerBox;
+use libp2p::core::transport::dummy::DummyTransport;
 use libp2p_gossipsub::{MessageId, PublishError};
 use log::{debug, error, info};
 use serde::{Deserialize, Serialize};
@@ -104,40 +106,6 @@ struct NodeBehaviour {
     mdns: mdns::tokio::Behaviour,
 }
 
-impl NodeBehaviour {
-    fn new(key: &libp2p::identity::Keypair) -> Self {
-        let message_id_fn = |message: &gossipsub::Message| {
-            let mut s = DefaultHasher::new();
-            message.data.hash(&mut s);
-            gossipsub::MessageId::from(s.finish().to_string())
-        };
-
-        // Set a custom gossipsub configuration
-        let gossipsub_config = gossipsub::ConfigBuilder::default()
-            .heartbeat_initial_delay(Duration::from_secs(1))
-            .history_length(10)
-            .history_gossip(10)
-            .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-            .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-            .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-            .build()
-            .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))
-            .unwrap(); // Temporary hack because `build` does not return a proper `std::error::Error`.
-
-        // build a gossipsub network behaviour
-        let gossipsub = gossipsub::Behaviour::new(
-            gossipsub::MessageAuthenticity::Signed(key.clone()),
-            gossipsub_config,
-        ).unwrap();
-
-        Self {
-            gossipsub,
-            mdns: mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())
-                .unwrap(),
-        }
-    }
-}
-
 enum Event {
     Gossipsub(gossipsub::Event),
     Mdns(mdns::Event),
@@ -169,17 +137,25 @@ impl Node {
     pub fn connect(&mut self, url: &str, topic_str: &str) {
         info!("Connecting to {} with topic {}", url, topic_str);
         let topic = gossipsub::IdentTopic::new(topic_str);
-        self.swarm
+        match self.swarm
             .behaviour_mut()
             .gossipsub
-            .subscribe(&topic)
-            .unwrap();
+            .subscribe(&topic) {
+            Ok(_) => {
+                // self.subscribed_topics.push(topic_str.to_string());
+                // debug!("{:?} Subscribed to topic {:?}", self.name, topic_str);
+            }
+            Err(e) => {
+                error!("{:?} Failed to subscribe to topic {:?}", self.name, e);
+            }
+        }
 
         self.swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse().unwrap()).unwrap();
     }
 
     pub fn broadcast(&mut self, message: Message) -> Result<Vec<MessageId>, PublishError> {
         let mut message_ids = vec![];
+        debug!("Broadcasting message: {:?}",  self.subscribed_topics);
         for topic in self.subscribed_topics.clone() {
             let topic = gossipsub::IdentTopic::new(topic);
 
@@ -190,9 +166,11 @@ impl Node {
                 .publish(topic, message.to_json().as_bytes())
             {
                 Ok(id) => {
+                    debug!("{:?} Broadcasted message: {:?}", self.name, id);
                     message_ids.push(id);
                 }
                 Err(e) => {
+                    error!("{:?} Failed to broadcast message: {:?}", self.name, e);
                     return Err(e);
                 }
             }
@@ -204,7 +182,7 @@ impl Node {
         match self.out_tx.clone().send(message).await {
             Ok(_) => {}
             Err(e) => {
-                error!("{:?} Failed to send message: {:?}", self.name, e);
+                error!("{:?} Failed to send message: {:?}", self.name, e.to_string());
             }
         };
     }
@@ -218,7 +196,6 @@ impl Node {
                         match self.broadcast(message){
                             Ok(message_ids) => {
                                 debug!("{:?} Broad casted message: {:?}", self.name, message_ids);
-
                             }
                             Err(e) => {
                                 error!("{:?} Failed to broadcast message: {:?}", self.name, e);
@@ -232,16 +209,16 @@ impl Node {
 
                 event = self.swarm.select_next_some() => match event {
                          SwarmEvent::NewListenAddr { address, .. } => {
-                            debug!("{:?} Listening on {:?}", self.name, address);
+                            debug!("{:?} NewListenAddr {:?}", self.name, address);
                             self.pass_message_to_node(Message::event(self.swarm.local_peer_id().to_string(),EventType::OnListen,)).await
 
                    },
                         SwarmEvent::ConnectionEstablished { peer_id, .. } => {
-                            debug!("{:?} Connected to {:?}", self.name, peer_id);
+                            debug!("{:?} ConnectionEstablished {:?}", self.id, peer_id);
                             self.pass_message_to_node(Message::event(self.swarm.local_peer_id().to_string(),EventType::OnConnectionEstablished,)).await
                         },
-                        SwarmEvent::ConnectionClosed { peer_id, .. } => {
-                            debug!("{:?} Disconnected from {:?}", self.name, peer_id);
+                        SwarmEvent::ConnectionClosed { peer_id, cause ,.. } => {
+                            debug!("{:?} ConnectionClosed {:?} {:?}", self.id, peer_id, cause);
                             self.pass_message_to_node(Message::event(self.swarm.local_peer_id().to_string(),EventType::OnConnectionClosed ,)).await
                         },
 
@@ -295,7 +272,41 @@ impl Node {
     }
 }
 
-pub fn create_node(
+
+impl NodeBehaviour {
+    fn new(key: &libp2p::identity::Keypair) -> Self {
+        let message_id_fn = |message: &gossipsub::Message| {
+            let mut s = DefaultHasher::new();
+            message.data.hash(&mut s);
+            gossipsub::MessageId::from(s.finish().to_string())
+        };
+
+        // Set a custom gossipsub configuration
+        let gossipsub_config = gossipsub::ConfigBuilder::default()
+            .history_length(10)
+            .history_gossip(10)
+            .heartbeat_interval(Duration::from_secs(1)) // This is set to aid debugging by not cluttering the log space
+            .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+            .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+            .build()
+            .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))
+            .unwrap(); // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+        // build a gossipsub network behaviour
+        let gossipsub = gossipsub::Behaviour::new(
+            gossipsub::MessageAuthenticity::Signed(key.clone()),
+            gossipsub_config,
+        ).unwrap();
+
+        Self {
+            gossipsub,
+            mdns: mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())
+                .unwrap(),
+        }
+    }
+}
+
+pub async fn create_node(
     name: String,
     in_rx: mpsc::Receiver<Message>,
 ) -> (Node, mpsc::Receiver<Message>) {
@@ -307,10 +318,31 @@ pub fn create_node(
             yamux::Config::default,
         ).unwrap()
         .with_quic()
-        .with_behaviour(|key| {
-            Ok(NodeBehaviour::new(key))
-        })
+        .with_other_transport(|_key| DummyTransport::<(PeerId, StreamMuxerBox)>::new())
         .unwrap()
+        .with_dns()
+        .unwrap()
+        .with_websocket(
+            (tls::Config::new, noise::Config::new),
+            yamux::Config::default,
+        )
+        .await
+        .unwrap()
+        .with_relay_client(
+            (tls::Config::new, noise::Config::new),
+            yamux::Config::default,
+        )
+        .unwrap()
+        .with_behaviour(|_key, relay| {
+            Ok(NodeBehaviour::new(_key))
+        })
+
+        .unwrap()
+        .with_swarm_config(|cfg| {
+            // Edit cfg here.
+            cfg
+                .with_idle_connection_timeout(Duration::from_secs(240))
+        })
         .build();
 
     let (out_tx, _rx) = mpsc::channel(100);
@@ -336,8 +368,9 @@ mod tests {
 
     use crate::node::node::{create_node, Message};
 
-    #[test]
-    fn test_ping() {
+
+    #[tokio::test]
+    async fn test_ping() {
         env_logger::init();
         let port_id = 8888;
         let topic = "test_topic";
@@ -347,18 +380,18 @@ mod tests {
         let (tx_0, mut rx_0) = tokio::sync::mpsc::channel::<Message>(100);
         let (tx_1, mut rx_1) = tokio::sync::mpsc::channel::<Message>(100);
 
-        let (mut node_0, mut rx_o_0) = create_node("node_0".to_string(), rx_0);
-        let (mut node_1, mut rx_o_1) = create_node("node_1".to_string(),  rx_1);
+        let (mut node_0, mut rx_o_0) = create_node("node_0".to_string(), rx_0).await;
+        let (mut node_1, mut rx_o_1) = create_node("node_1".to_string(), rx_1).await;
 
         let node_0_id = node_0.id.clone();
         let node_1_id = node_1.id.clone();
 
-        let runtime = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+        // let runtime = tokio::runtime::Builder::new_current_thread()
+        //     .enable_all()
+        //     .build()
+        //     .unwrap();
 
-        runtime.spawn(async move {
+        tokio::spawn(async move {
             while let Some(message_data) = rx_o_0.recv().await {
                 debug!("Node_0 Received: {:?}", message_data);
                 let msg = Message::data(
@@ -376,7 +409,7 @@ mod tests {
             }
         });
 
-        runtime.spawn(async move {
+        tokio::spawn(async move {
             while let Some(message_data) = rx_o_1.recv().await {
                 debug!("Node_1 Received: {:?}", message_data);
                 let msg = Message::data(
@@ -394,17 +427,14 @@ mod tests {
             }
         });
         let url_ = url.clone();
-        runtime.spawn(async move {
+        tokio::spawn(async move {
             node_0.connect(url_.clone().as_str(), topic);
             node_0.run().await;
         });
         let url_ = url.clone();
-        runtime.block_on(async move {
-            // wait for 1 event to make sure swarm0 is listening
-            tokio::time::sleep(std::time::Duration::from_millis(10000)).await;
+        tokio::time::sleep(std::time::Duration::from_millis(10000)).await;
 
-            node_1.connect(url_.clone().as_str(), topic);
-            node_1.run().await;
-        });
+        node_1.connect(url_.clone().as_str(), topic);
+        node_1.run().await;
     }
 }
