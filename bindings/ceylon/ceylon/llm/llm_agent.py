ae1387d2e2e0747275f0cb9ee25c9b69e6eceac5
dewmal <dewmalnilanka@gmail.com>
2024-06-30 21:37:49 +0530

    Revert "Add network graph"
    This reverts commit 5819a1a0caa5d3b16e0671ae810a33ad383c8879.

--- bindings/ceylon/ceylon/llm/llm_agent.py
+++ bindings/ceylon/ceylon/llm/llm_agent.py
@@ -1,32 +1,24 @@
+import asyncio
 import pickle
-from collections import deque
+import random
 
-import networkx as nx
 from langchain_core.tools import StructuredTool
 
 from ceylon.ceylon import AgentCore, Processor, MessageHandler, AgentDefinition
-from ceylon.runner import RunnerInput
 
 
 class LLMAgent(AgentCore, MessageHandler, Processor):
     tools: list[StructuredTool]
-    network_graph: nx.DiGraph
-    queue: deque
 
     def __init__(self, name, position, instructions, responsibilities, llm, tools: list[StructuredTool] = None):
+        self.llm = llm
+        self.tools = tools
         super().__init__(definition=AgentDefinition(
             name=name,
             position=position,
             instructions=instructions,
             responsibilities=responsibilities
         ), on_message=self, processor=self)
-        self.llm = llm
-        self.tools = tools
-        # Create a directed graph to represent the workflow
-        self.network_graph = nx.DiGraph()
-
-        # Initialize the queue and executed agents
-        self.queue = deque()
 
     async def on_message(self, agent_id, data, time):
         definition = await self.definition()
@@ -34,42 +26,11 @@ class LLMAgent(AgentCore, MessageHandler, Processor):
         print(definition.id, agent_id, dt, time)
 
     async def run(self, inputs):
-        inputs: RunnerInput = pickle.loads(inputs)
         definition = await self.definition()
-
-        print(f"Agent {definition.name} run", inputs)
-
-        self._initialize_graph(inputs.network)
-
-    def _initialize_graph(self, network):
-        # Add nodes and edges based on the agents and their dependencies
-        for agent, dependencies in network.items():
-            self.network_graph.add_node(agent)
-            for dependency in dependencies:
-                self.network_graph.add_edge(dependency, agent)
-
-        # Initialize the queue with nodes that have no dependencies (indegree 0)
-        self.queue.extend([node for node in self.network_graph if self.network_graph.in_degree(node) == 0])
-
-        print(self.queue)
-
-    # def update_status(self, agent):
-    #     if agent not in self.queue:
-    #         print(f"Agent {agent} is not ready to execute or has already been executed.")
-    #         return
-    #
-    #     self.queue.remove(agent)
-    #     self.executed.append(agent)
-    #     print(f"Executing {agent}")
-    #
-    #     # Remove the current agent and update the graph
-    #     for successor in list(self.network_graph.successors(agent)):
-    #         self.network_graph.remove_edge(agent, successor)
-    #         if self.network_graph.in_degree(successor) == 0:
-    #             self.queue.append(successor)
-    #     self.network_graph.remove_node(agent)
-    #
-    #     if not self.network_graph.nodes:
-    #         print("Workflow executed successfully.")
-    #     elif not self.queue:
-    #         print("Cycle detected in the workflow!")
+        print(f"{definition.name} run", inputs)
+        while True:
+            await self.broadcast(pickle.dumps({
+                "title": "How to use AI for Machine Learning",
+                "sender": definition.name
+            }))
+            await asyncio.sleep(random.randint(1, 10))
