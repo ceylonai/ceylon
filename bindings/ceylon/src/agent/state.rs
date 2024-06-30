606e0ce72913fbb82359e447fd391fbd758d3c19
dewmal <dewmal@syigen.com>
2024-06-30 08:32:40 +0530

    Update state on message

--- bindings/ceylon/src/agent/state.rs
+++ bindings/ceylon/src/agent/state.rs
@@ -1,8 +1,11 @@
 use std::collections::{HashMap, HashSet};
 use std::time::{SystemTime, UNIX_EPOCH};
+use log::info;
 use serde::{Deserialize, Serialize};
 use tokio::sync::RwLock;
 
+
+pub static SYSTEM_MESSAGE_CONTENT_TYPE: &str = "ceylon.system.message.content";
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Message {
     pub id: String,
@@ -43,6 +46,15 @@ impl SystemMessage {
             SystemMessage::Beacon { .. } => "beacon".to_string(),
         }
     }
+    pub fn get_type(&self) -> String {
+        match self {
+            SystemMessage::Content(..) => SYSTEM_MESSAGE_CONTENT_TYPE.to_string(),
+            SystemMessage::SyncRequest { .. } => "sync_request".to_string(),
+            SystemMessage::SyncResponse { .. } => "sync_response".to_string(),
+            SystemMessage::Ack { .. } => "ack".to_string(),
+            SystemMessage::Beacon { .. } => "beacon".to_string(),
+        }
+    }
 
     pub fn to_bytes(&self) -> Vec<u8> {
         serde_json::to_vec(self).unwrap()
@@ -53,8 +65,17 @@ impl SystemMessage {
     }
 }
 
+pub type AgentStateMessageList = Vec<Message>;
+
+#[derive(Debug)]
+pub struct AgentStateSnap {
+    pub messages: AgentStateMessageList,
+    pub last_version: u128,
+}
+
+#[derive(Default)]
 pub struct AgentState {
-    messages: RwLock<Vec<Message>>,
+    messages: RwLock<AgentStateMessageList>,
     last_version: RwLock<u128>,
 }
 
@@ -67,13 +88,17 @@ impl AgentState {
     }
 
     pub async fn add_message(&self, message: Message) {
+        info!("Started add_message ordered by version");
         self.messages.write().await.push(message);
         self.order_by_version().await;
         let last_message = self.get_last_message().await;
-        let last_version = self.last_version.read().await;
-        if last_version.lt(&last_message.version) {
-            *self.last_version.write().await = last_message.version;
+        let last_version = self.last_version.read().await.clone();
+        info!( "Last version: {}, Last message version: {}", last_version, last_message.version);
+        if last_message.version > last_version {
+            let mut last_version = self.last_version.write().await;
+            *last_version = last_message.version;
         }
+        info!("Finished add_message ordered by version");
     }
 
     pub async fn get_last_message(&self) -> Message {
@@ -82,6 +107,7 @@ impl AgentState {
     }
 
     pub async fn order_by_version(&self) {
+        info!("Started updating ordered by version");
         let mut messages = self.messages.read().await.clone();
         let mut ordered_messages = Vec::new();
         messages.sort_by(|a, b| a.version.cmp(&b.version));
@@ -91,5 +117,12 @@ impl AgentState {
         let mut update_messages = self.messages.write().await;
         update_messages.clear();
         update_messages.extend(ordered_messages);
+        info!("Finished updating ordered by version");
+    }
+
+    pub async fn request_snapshot(&self) -> AgentStateSnap {
+        let messages = self.messages.read().await.clone();
+        let last_version = self.last_version.read().await.clone();
+        AgentStateSnap { messages, last_version }
     }
 }
