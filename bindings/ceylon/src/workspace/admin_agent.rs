e3672963896e3edaf325ffbd0850c36b06d5723b
dewmal <dewmal@syigen.com>
2024-07-15 08:08:10 +0530

    impl stop

--- bindings/ceylon/src/workspace/admin_agent.rs
+++ bindings/ceylon/src/workspace/admin_agent.rs
@@ -1,8 +1,9 @@
 use sangedama::peer::message::data::NodeMessage;
 use sangedama::peer::node::{AdminPeer, AdminPeerConfig};
-use tokio::select;
-use tokio::sync::mpsc::Receiver;
-use tracing::info;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
+use tokio::{select, signal};
+use tracing::{debug, info};
 
 #[derive(Clone)]
 pub struct AdminAgentConfig {
@@ -15,23 +16,33 @@ pub struct AdminAgent {
 
 impl AdminAgent {
     pub fn new(config: AdminAgentConfig) -> AdminAgent {
+
         AdminAgent {
-            config
+            config,
         }
     }
 
     pub async fn run(&self, inputs: Vec<u8>) {
+        self.run_(inputs).await;
+    }
+    pub async fn run_(&self, inputs: Vec<u8>) {
         info!("Workspace {} running", self.config.name);
 
         let config = self.config.clone();
         let admin_config = AdminPeerConfig::new(config.port, config.name.clone());
         let (mut peer_, mut peer_listener_) = AdminPeer::create(admin_config.clone()).await;
 
-
         let admin_id = peer_.id.clone();
         let admin_emitter = peer_.emitter();
 
+        let mut is_request_to_shutdown = false;
+
+
+        let name = self.config.name.clone();
         loop {
+            if is_request_to_shutdown {
+                break;
+            }
             select! {
                event = peer_listener_.recv() => {
                     if event.is_some() {
@@ -46,7 +57,17 @@ impl AdminAgent {
                         }
                     }
                 }
+
+                 _ = signal::ctrl_c() => {
+                    println!("Agent {:?} received exit signal", name);
+                    // Perform any necessary cleanup here
+                    is_request_to_shutdown = true;
+                },
             }
         }
+    }
+
+    pub async fn stop(&self) {
+        info!("Agent {} stop called", self.config.name);
     }
 }
