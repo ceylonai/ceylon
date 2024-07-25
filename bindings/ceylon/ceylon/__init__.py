b61aaf0abb5567227352379815f8ad0040d3d958
dewmal <dewmal@syigen.com>
2024-07-26 00:52:58 +0530

    print ceylon details

--- bindings/ceylon/ceylon/__init__.py
+++ bindings/ceylon/ceylon/__init__.py
@@ -2,3 +2,7 @@
 from .agent.agents import RunnerAgent, Agent
 from .agent.types.job import AgentJobResponse, AgentJobStepRequest
 from .agent.types.job import JobRequest, JobSteps, Step
+from .ceylon import version
+
+print(f"ceylon version: {version()}")
+print(f"visit https://ceylon.ai for more information")
