6e1330386ac83968d2168d51942a00077dafc796
dewmal <dewmal@syigen.com>
2024-08-06 06:20:36 +0530

    remove

--- bindings/ceylon/tests/test_worker/remote/remote_worker.py
+++ bindings/ceylon/tests/test_worker/remote/remote_worker.py
@@ -13,8 +13,8 @@ class WorkerAgent1(Agent):
         logger.info((f"WorkerAgent1 on_run  {self.details().name}", inputs))
 
 
-worker_1 = WorkerAgent1("worker_2", "server_admin", admin_port=8000,
-                        admin_peer="12D3KooWJCAL6SbHPLdyga6ENyvr7M1WpEhcBWEqDs5xFzA1BNq3")
+worker_1 = WorkerAgent1("worker_2", "server_admin", role="What enver", admin_port=8000,
+                        admin_peer="12D3KooWFTTKVN48Ps5DYmUX6YY16Zjo5gm7NbBD65geFohuv5ke")
 
 
 async def run():
@@ -22,5 +22,5 @@ async def run():
     await worker_1.start(pickle.dumps({}))
 
 
-enable_log("info")
+# enable_log("info")
 asyncio.run(run())
