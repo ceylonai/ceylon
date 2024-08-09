22febb98e67bbe1d59b42d47dbc873f710b1814b
dewmal <dewmal@syigen.com>
2024-08-09 09:25:28 +0530

    Update comments

--- bindings/ceylon/ceylon/agent/types/agent_request.py
+++ bindings/ceylon/ceylon/agent/types/agent_request.py
@@ -8,7 +8,7 @@ from .job_step import Step
 
 class AgentJobStepRequest(BaseModel):
     """ the agent job step request"""
-    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias='_id')
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias='_id', description="the id of the request")
     job_id: str = Field(None, description="the job id")
     worker: str = Field(description="the worker name")
     job_data: Any = Field(None, description="the job data")
@@ -16,8 +16,11 @@ class AgentJobStepRequest(BaseModel):
 
 
 class AgentJobResponse(BaseModel):
-    """ the agent job response"""
-    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias='_id')
-    job_id: str = Field(None, description="the job id")
+    """
+    the agent job response
+    this is the response that is sent back to the agent
+    """
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias='_id', description="the id of the response")
+    job_id: str = Field(None, description="the job id of the response")
     worker: str = Field(description="the worker name")
     job_data: Any = Field(None, description="the job data")
