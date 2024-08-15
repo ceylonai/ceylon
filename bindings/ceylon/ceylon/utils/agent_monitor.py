9f410bc749f5d94a76cff194a6372af3c7bd655e
dewmal <dewmal@syigen.com>
2024-08-15 06:02:37 +0530

    introduce monitor

---
+++ bindings/ceylon/ceylon/utils/agent_monitor.py
@@ -0,0 +1,26 @@
+import pickle
+
+from loguru import logger
+
+from ceylon import Agent
+from ceylon.static_val import DEFAULT_WORKSPACE_ID, DEFAULT_ADMIN_PORT
+
+
+class AgentMonitor(Agent):
+
+    def __init__(self, name="monitor",
+                 workspace_id=DEFAULT_WORKSPACE_ID,
+                 admin_port=DEFAULT_ADMIN_PORT,
+                 admin_peer="",
+                 role="worker"):
+        super().__init__(
+            name=name,
+            workspace_id=workspace_id,
+            admin_port=admin_port,
+            admin_peer=admin_peer,
+            role=role if role else name
+        )
+
+    async def on_message(self, agent_id: "str", data: "bytes", time: "int"):
+        data = pickle.loads(data)
+        logger.info((agent_id, data, time))
