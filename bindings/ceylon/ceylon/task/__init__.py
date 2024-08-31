84c50e347a1e140259ae66c1dda4b5d8bc39f6e4
dewmal <dewmal@syigen.com>
2024-08-31 10:05:39 +0530

    Update admin to write network ip address

--- bindings/ceylon/ceylon/task/__init__.py
+++ bindings/ceylon/ceylon/task/__init__.py
@@ -1,1 +1,3 @@
 from .task_operation import SubTask, SubTaskResult, TaskAssignment, Task
+from .task_coordinator import TaskCoordinator
+from .task_operator import TaskOperator
--- bindings/ceylon/src/utils/env.rs
+++ bindings/ceylon/src/utils/env.rs
@@ -1,11 +1,8 @@
 use std::collections::HashMap;
-use std::fs::OpenOptions;
+use std::fs::File;
 use std::io::Write;
 pub(crate) fn write_to_env_file(env_vars: &HashMap<String, String>) -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(".ceylon_network")?;
+    let mut file = File::create(".ceylon_network")?;
 
     for (key, value) in env_vars {
         writeln!(file, "{}={}", key, value)?;
