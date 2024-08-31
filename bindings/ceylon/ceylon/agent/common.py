d5fbf2ede00e770fbe81c44cf3ba0d43bd3194e2
dewmal <dewmal@syigen.com>
2024-08-31 18:08:05 +0530

    Update common to handle not available messages

--- bindings/ceylon/ceylon/agent/common.py
+++ bindings/ceylon/ceylon/agent/common.py
@@ -31,30 +31,34 @@ class AgentCommon:
 
     async def _on_message_handler(self, agent_id: "str", data: "bytes", time: "int"):
         # Deserialize the message
-        message = pickle.loads(data)
-        message_type = type(message)
+        try:
+            message = pickle.loads(data)
+            message_type = type(message)
 
-        # Get the class hierarchy
-        class_hierarchy = inspect.getmro(self.__class__)
+            # Get the class hierarchy
+            class_hierarchy = inspect.getmro(self.__class__)
 
-        # Find the most specific handler in the class hierarchy
-        handler = None
-        for cls in class_hierarchy:
-            class_name = cls.__name__
-            method_key = f"{class_name}.{message_type}"
-            if method_key in message_handlers:
-                handler = message_handlers[method_key]
-                break
+            # Find the most specific handler in the class hierarchy
+            handler = None
+            for cls in class_hierarchy:
+                class_name = cls.__name__
+                method_key = f"{class_name}.{message_type}"
+                if method_key in message_handlers:
+                    handler = message_handlers[method_key]
+                    break
 
-        # Trigger the appropriate handler if one is registered
-        if handler:
-            if has_param(handler, "agent_id") and has_param(handler, "time"):
-                await handler(self, message, agent_id=agent_id, time=time)
-            elif has_param(handler, "agent_id"):
-                await handler(self, message, agent_id=agent_id)
-            elif has_param(handler, "time"):
-                await handler(self, message, time=time)
+            # Trigger the appropriate handler if one is registered
+            if handler:
+                if has_param(handler, "agent_id") and has_param(handler, "time"):
+                    await handler(self, message, agent_id=agent_id, time=time)
+                elif has_param(handler, "agent_id"):
+                    await handler(self, message, agent_id=agent_id)
+                elif has_param(handler, "time"):
+                    await handler(self, message, time=time)
+                else:
+                    await handler(self, message)
             else:
-                await handler(self, message)
-        else:
-            logger.warning(f"No handler registered in the class hierarchy for message type: {message_type}")
+                logger.warning(f"No handler registered in the class hierarchy for message type: {message_type}")
+
+        except Exception as e:
+            logger.error(e)
