8cd8b93be26f63b5623ee98f4251c18337735e98
Dewmal <dewmal@syigen.com>
2025-01-19 10:45:19 +0530

    add copyright notice

--- bindings/ceylon/src/lib.rs
+++ bindings/ceylon/src/lib.rs
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2023-2025 SYIGEN LTD.
+ * Author: Dewmal - dewmal@syigen.com
+ * Created: 2025-01-19
+ * Ceylon Project - https://github.com/ceylonai/ceylon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file is part of Ceylon Project.
+ * Original authors: Dewmal - dewmal@syigen.com
+ * For questions and support: https://github.com/ceylonai/ceylon/issues
+ */
+
+
 fn version() -> String {
     env!("CARGO_PKG_VERSION").to_string()
 }
