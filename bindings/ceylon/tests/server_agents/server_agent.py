48a77a46457d0578b9c1dd637b71455f513077a9
Dewmal <dewmal@syigen.com>
2025-01-19 20:08:12 +0530

    remove old code from repo

--- bindings/ceylon/tests/server_agents/server_agent.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-from ceylon.ceylon import enable_log
-from ceylon.task import TaskCoordinator
-
-task_manager = TaskCoordinator(tasks=[], agents=[])
-
-enable_log("INFO")
-asyncio.run(task_manager.async_do())
